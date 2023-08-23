7cb71ff9456f9b3fac5c8f720029f4bf7f68d744
renawmontanari <regiangelica@gmail.com>
2023-08-23 00:14:40 -0300

    miniblog

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+// Context
+import { AuthProvider } from './context/AuthContext';
+
 // Pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
@@ -16,18 +19,20 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
+     <AuthProvider>
       <BrowserRouter>
-      <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+        <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+     </AuthProvider>
     </div>
   );
 };
