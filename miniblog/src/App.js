ffe2e8b25383f33d5bfb0ac9bdc76604a13833f7
renawmontanari <regiangelica@gmail.com>
2023-08-23 00:32:29 -0300

    miniblog

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -2,6 +2,11 @@
 import './App.css';
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from 'firebase/auth';
+
+// Hooks
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
 
 // Context
 import { AuthProvider } from './context/AuthContext';
@@ -17,9 +22,27 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
+
+  const [user, setUser] = useState(undefined);
+  const {auth} = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+  }, [auth]);
+
+  if(loadingUser) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-     <AuthProvider>
+     <AuthProvider value={{ user }}>
       <BrowserRouter>
         <Navbar />
           <div className="container">
