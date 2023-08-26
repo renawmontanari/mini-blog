881a2b3617f7d22ed40aefc15dfe5ccc7f4349d0
renawmontanari <regiangelica@gmail.com>
2023-08-26 13:53:28 -0300

    miniblog

--- miniblog/src/pages/Register/Register.js
+++ miniblog/src/pages/Register/Register.js
@@ -91,7 +91,11 @@ const Register = () => {
             />
           </label>
           {!loading && <button className="btn">Cadastrar</button>}
-          {loading && <button className="btn" disabled>Aguarde...</button>}
+          {loading && (
+            <button className="btn" disabled>
+              Aguarde...
+            </button>
+          )}
           {error && <p className="error">{error}</p>}
         </form>
     </div>
