11ff7e9260d85d227b1b6369f8f094a17a4b42fc
renawmontanari <regiangelica@gmail.com>
2023-08-28 23:38:46 -0300

    miniblog

--- miniblog/src/pages/CreatePost/CreatePost.js
+++ miniblog/src/pages/CreatePost/CreatePost.js
@@ -17,26 +17,40 @@ const CreatePost = () => {
 
   const { insertDocument, response } = useInsertDocument("posts");
 
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError("");
 
     // Validar image URL
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
 
     // Criar o array de tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
 
     // Checar todos os valores
+    if (!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos!");
+    }
+
+    if (formError) return;
 
     insertDocument({
       title,
       image,
       body,
-      tags,
+      tagsArray,
       uid: user.uid,
       createdBy: user.displayName
     });
 
     // Redirecionar para a página Home
+    navigate("/");
 
   };
 
@@ -95,6 +109,7 @@ const CreatePost = () => {
             </button>
           )}
           {response.error && <p className="error">{response.error}</p>}
+          {formError && <p className="error">{formError}</p>}
         </form>
     </div>
   );
