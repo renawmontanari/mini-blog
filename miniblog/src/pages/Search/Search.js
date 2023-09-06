6281c52fc5d99c9009892ac2d32f7b3dd24edc63
renawmontanari <regiangelica@gmail.com>
2023-09-06 00:17:41 -0300

    miniblog

--- miniblog/src/pages/Search/Search.js
+++ miniblog/src/pages/Search/Search.js
@@ -1,19 +1,37 @@
-import React from "react";
+
+// CSS
+import styles from "./Search.module.css";
 
 // Hooks
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useQuery } from "../../hooks/useQuery";
+import { Link } from "react-router-dom";
+
+// Components
+import PostDetails from "../../components/PostDetails";
 
 const Search = () => {
     const query = useQuery();
     const search = query.get("q");
 
-    const {documents: posts} = useFetchDocuments("posts");
+    const { documents: posts } = useFetchDocuments("posts", search);
 
   return (
-    <div>
+    <div className={styles.search__container}>
         <h2>Search</h2>
-        <p>{search}</p>
+        <div>
+          {posts && posts.length === 0 && (
+            <>
+              <p>Não foram encontrados posts a partir da sua busca...</p>
+              <Link to="/" className="btn btn__dark">
+                Voltar
+              </Link>
+            </>
+          )}
+          {posts && posts.map((post) => 
+            <PostDetails key={post.id} post={post} />
+          )}
+        </div>
     </div>
   );
 };
